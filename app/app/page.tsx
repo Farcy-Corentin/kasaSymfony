9abc8f78869f0029b5d5192006174272f7faed8c
Corentin <corentinfarcy1@gmail.com>
2024-07-17 16:57:49 +0200

    init project

---
+++ app/app/page.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from '@mui/material'
+
+export default function Home() {
+  return (
+    <Box
+      component="main"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Typography variant="h1">Hello, World!</Typography>
+    </Box>
+  )
+}
